47058825da04dd1a074877322923a4c2a62da72b
moureira <ericmoureira35@gmail.com>
2025-08-15 15:43:05 -0300

    feat: atualizar autor por id finalizado

--- src/controllers/autorControllers.js
+++ src/controllers/autorControllers.js
@@ -3,33 +3,33 @@ import autorModel from '../models/autorModel.js';
 export const cadastrarAutor = async (req, res) => {
     const { nome, biografia, data_nascimento, nacionalidade } = req.body;
 
-    if (!nome){
+    if (!nome) {
         res.status(400).json({
             erro: 'Campo nome invalido',
             mensagem: 'O campo nome não pode ser nulo'
         });
         return
     }
-    if (!biografia){
+    if (!biografia) {
         res.status(400).json({
             erro: 'Campo biografia invalido',
             mensagem: 'O campo biografia não pode ser nulo'
         });
         return
     }
-    if (!data_nascimento){
-       res.status(400).json({
+    if (!data_nascimento) {
+        res.status(400).json({
             erro: 'Campo data_nascimento invalido',
             mensagem: 'O campo data_nascimento não pode ser nulo'
-       });
-       return
+        });
+        return
     }
-    if (!nacionalidade){
-       res.status(400).json({
-        erro: 'Campo nacionalidade invalido',
-        mensagem: 'O campo nacionalidade não pode ser nulo'
-       });
-       return
+    if (!nacionalidade) {
+        res.status(400).json({
+            erro: 'Campo nacionalidade invalido',
+            mensagem: 'O campo nacionalidade não pode ser nulo'
+        });
+        return
     }
 
     const validaData = new Date(data_nascimento);
@@ -50,9 +50,9 @@ export const cadastrarAutor = async (req, res) => {
 
     try {
         const novoAutor = autorModel.create(autor);
-        res.status(201).json({mensagem: "autor criado com sucesso", novoAutor});
+        res.status(201).json({ mensagem: "autor criado com sucesso", novoAutor });
     } catch (error) {
-        res.status(500).json({mensagem: 'Erro ao cadastrar autor'});
+        res.status(500).json({ mensagem: 'Erro ao cadastrar autor' });
     }
 }
 
@@ -62,32 +62,51 @@ export const listarTodosAutores = async (req, res) => {
     const offset = (page - 1) * limit
 
     try {
-        const autores = await autorModel.findAndCountAll({offset, limit})
+        const autores = await autorModel.findAndCountAll({ offset, limit })
         const totalPaginas = Math.ceil(autores.count / limit)
-        res.status(200).json({totalAutores: autores.count, totalPaginas, paginalAtual: page, autoresPorPagina: limit, autores: autores.rows});
+        res.status(200).json({ totalAutores: autores.count, totalPaginas, paginalAtual: page, autoresPorPagina: limit, autores: autores.rows });
     } catch (error) {
         console.log(error)
-        res.status(500).json({mensagem: 'Erro ao listar autores'});
-        
-    } 
+        res.status(500).json({ mensagem: 'Erro ao listar autores' });
+
+    }
     res.send('Listar todos os autores');
 }
 
 export const buscarAutorPorId = async (req, res) => {
-    const {id} = req.params
+    const { id } = req.params
 
     if (!id) {
-        res.status(400).json({mensagem: "ID tem que ser valido"})
+        res.status(400).json({ mensagem: "ID tem que ser valido" })
     }
 
     try {
         const autor = await autorModel.findByPk(id)
         if (!autor) {
-            res.status(400).json({mensagem: "autor não encontrado"})
+            res.status(400).json({ mensagem: "autor não encontrado" })
             return
         }
         res.status(200).json(autor)
     } catch (error) {
-        res.status(500).json({mensagem: 'Erro ao listar autor'})
+        res.status(500).json({ mensagem: 'Erro ao listar autor' })
+    }
+}
+
+export const atualizarAutor = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { nome, biografia, dataNascimento, nacionalidade } = req.body;
+
+        const autor = await autorModel.findByPk(id);
+
+        if (!autor) {
+            return res.status(404).json({ mensagem: "Autor não encontrado!" });
+        }
+
+        await autor.update({ nome, biografia, dataNascimento, nacionalidade });
+
+        res.status(200).json(autor);
+    } catch (error) {
+        res.status(400).json({ message: 'Erro ao atualizar o autor', error: error.message });
     }
 }
--- src/routes/autorRoutes.js
+++ src/routes/autorRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express"
-import { buscarAutorPorId, cadastrarAutor, listarTodosAutores } from "../controllers/autorControllers.js"
+import { atualizarAutor, buscarAutorPorId, cadastrarAutor, listarTodosAutores } from "../controllers/autorControllers.js"
 
 const router = Router()
 
 router.post('/', cadastrarAutor)
 router.get('/', listarTodosAutores)
 router.get('/:id', buscarAutorPorId)
+router.put('/:id', atualizarAutor)
 
 export default router
